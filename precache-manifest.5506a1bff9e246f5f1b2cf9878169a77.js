609f85d4a262c1bcada98dfd56c28407eb4699a5
Joseph Chamul <josephchamul@Josephs-MacBook-Pro.local>
2020-10-08 10:57:42 -0700

    Updates

---
+++ precache-manifest.5506a1bff9e246f5f1b2cf9878169a77.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f1c02836ab37781203cbdabc28bc92",
+    "url": "/imagequiz/index.html"
+  },
+  {
+    "revision": "c2b3423fdeafd0f3e783",
+    "url": "/imagequiz/static/css/main.b8012448.chunk.css"
+  },
+  {
+    "revision": "e3d8992116123d5fff3b",
+    "url": "/imagequiz/static/js/2.a068b877.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imagequiz/static/js/2.a068b877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2b3423fdeafd0f3e783",
+    "url": "/imagequiz/static/js/main.dfbb1f66.chunk.js"
+  },
+  {
+    "revision": "85da51fa7abe07fb0938",
+    "url": "/imagequiz/static/js/runtime-main.2f3f5ace.js"
+  },
+  {
+    "revision": "b610ae10f73384b410deb8af2309de86",
+    "url": "/imagequiz/static/media/cherryblossom.b610ae10.png"
+  },
+  {
+    "revision": "88848efd7467dd193652b4b3b6478974",
+    "url": "/imagequiz/static/media/daffodil.88848efd.png"
+  },
+  {
+    "revision": "6f79caebdf34f3c20a49c30701002ad8",
+    "url": "/imagequiz/static/media/rose.6f79caeb.png"
+  },
+  {
+    "revision": "adc45e53918ca3fa49e002fa275e9b9b",
+    "url": "/imagequiz/static/media/sunflower.adc45e53.png"
+  },
+  {
+    "revision": "f9a3f0da6c51db0c853536791e7522c8",
+    "url": "/imagequiz/static/media/tulip.f9a3f0da.png"
+  },
+  {
+    "revision": "fca50ba10a4ca041341a13d9f650a22f",
+    "url": "/imagequiz/static/media/waterlily.fca50ba1.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/imagequiz/precache-manifest.c5be4b5f7c1299ae4482327a5654e150.js"
+  "/imagequiz/precache-manifest.5506a1bff9e246f5f1b2cf9878169a77.js"
 );
 
 self.addEventListener('message', (event) => {
